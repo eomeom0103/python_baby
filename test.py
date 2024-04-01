f38c906fdc435bb0d1b6cef8a4bba0dabee7ce03
eomeom0103 <84564186+eomeom0103@users.noreply.github.com>
2024-04-01 22:02:43 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,41 @@
+# import antigravity
+# 만화 나오는 이스터 에그
+
+"""
+print 연습
+a = 3
+print(a)
+"""
+
+"""
+print('Name : ')
+name=input()
+print('Name :',name)
+"""
+
+# 주석 작성
+#를 사용하면 주석됨. 여러 줄은 """ 코드 """
+# 주석 단축키 : Ctrl + /
+
+# 포맷팅? %d %f %s 등으로 값 받아오는거
+
+# print('%d %d' % (1,2))
+
+# a,b=(1,2)
+# print(a, b)
+
+# format 메서드
+# print('a is {}'.format(4))
+# print('{} x {} = {}'.format(2,3,2*3))
+# print('{1} x {2} = {0}'.format(6,3,2)) # {}안의 숫자가 format() 안에 있는 숫자들의 index
+
+# 문자열 인덱싱
+# str = "윤동원 바보"
+# print(str[2]) # 인덱스는 0부터 시작하는거 유의
+# print(str[-1]) # 마이너스 주소는 맨 끝 문자가 -1 그 앞은 -2 등등 쭉 나감
+
+# 문자열 slicing
+# str = "윤동원 바보"
+# print(str[2:5]) # 문자들이 네모칸 안에 들어있다고 생각하고 네모칸의 경계선을 0 1 2 3이라 생각하면 안헷갈림
+# print(str[:5])  # 범위값을 생략하면 처음부터 or 끝까지 싹 다 나옴
+
